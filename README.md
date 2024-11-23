fa16900cd9af3e037368613677c111828e619cb7
i-icc <ichi623110@gmail.com>
2024-11-23 09:52:27 +0900

    add aws tf file

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # menu
 自分専用のレシピ・メニュー表
+
+## dir 構成
+```
+.
+├── .github
+│   └── workflows
+├── src
+│   └── menu-client
+└── terraform
+```
+* src
+  * アプリケーションファイル
+  * menu-client
+    * クライアントソース
+* terraform
+  * IaC 周りのコード
+* .github
+  * terraform
+    * actions のファイル
+
+## deploy
+* main へのマージで自動デプロイ
+  * テスト環境は特になし
+  * とりあえず勉強も兼ねて AWS へ
+    * お金もかかるのでしばらくしたら GithubPages か Vercel に乗り換える
---
+++ terraform/main.tf
@@ -0,0 +1,98 @@
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
+variable "region" {
+  description = "The name of the aws region"
+  type        = string
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_s3_bucket" "static_site" {
+  bucket = var.bucket_name
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Name        = "StaticSite"
+    Environment = "Production"
+  }
+}
+
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = "Origin Access Identity for CloudFront"
+}
+
+resource "aws_s3_bucket_policy" "static_site_policy" {
+  bucket = aws_s3_bucket.static_site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          AWS = aws_cloudfront_origin_access_identity.oai.iam_arn
+        },
+        Action   = "s3:GetObject",
+        Resource = "${aws_s3_bucket.static_site.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_cloudfront_distribution" "static_site_cdn" {
+  origin {
+    domain_name = aws_s3_bucket.static_site.bucket_regional_domain_name
+    origin_id   = "S3-static-site"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-static-site"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  price_class = "PriceClass_100"
+}
+
+output "cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.static_site_cdn.domain_name
+}
