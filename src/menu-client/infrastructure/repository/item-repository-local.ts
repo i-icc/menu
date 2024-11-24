7a11d4f73e52a718ebc3fafbba74c274c4ba30d4
i-icc <ichi623110@gmail.com>
2024-11-24 20:28:20 +0900

    #1 ファイルからデータを受け取るように修正

--- src/menu-client/infrastructure/repository/item-repository-local.ts
+++ src/menu-client/infrastructure/repository/item-repository-local.ts
@@ -1,89 +1,73 @@
 import { tags } from '@/data/menu';
 import { MenuItem, Tag } from '@/types/menu';
 import { ItemRepository } from '@/types/repository';
+import menuJson from '@/public/data/menu.json';
 
+type itemDto = {
+    id: string;
+    name: string;
+    description: string;
+    image: string;
+    steps: string[];
+};
+
+type tagRelationDto = {
+    item_id: string;
+    tag_id: string;
+}
 // リポジトリインターフェース
 export class ItemRepositoryLocal implements ItemRepository {
-    async getAllItems(): Promise<MenuItem[]> {
-        const menuItems: MenuItem[] = [
-            {
-                id: '1',
-                name: 'パエリア',
-                description: '',
-                image: '/images/4A29FF84.png',
-                steps: [],
-                tags: [tags['4A29FF84']],
-            },
-            {
-                id: '2',
-                name: '親子丼',
-                description: 'Sourdough bread topped with mashed avocado, poached eggs, and microgreens',
-                image: '/images/4A29FF84.png',
-                steps: [],
-                tags: [tags['4A29FF84']],
-            },
-            {
-                id: '3',
-                name: 'Tiramisu',
-                description: 'Classic Italian dessert with layers of coffee-soaked ladyfingers and mascarpone cream',
-                image: '/images/4A29FF84.png',
-                steps: [],
-                tags: [tags['4A29FF84']],
-            }
-        ];
-        return menuItems;
-    }
+    private itemList: MenuItem[];
+    private tagItemMap;
 
-    async getItemById(id: string): Promise<MenuItem | null> {
-        return {
-            id: id,
-            name: 'Tiramisu',
-            description: 'Classic Italian dessert with layers of coffee-soaked ladyfingers and mascarpone cream',
-            image: '/images/4A29FF84.png',
-            steps: [],
-            tags: [tags['4A29FF84']],
+    constructor() {
+        try {
+            const itemData: itemDto[] = menuJson["items"];
+            const item_tag_relations: tagRelationDto[] = menuJson["item_tag_relation"]
+            const itemTagMap = item_tag_relations.reduce((acc, relation) => {
+                if (!acc[relation.item_id]) {
+                    acc[relation.item_id] = [];
+                }
+                acc[relation.item_id].push(relation.tag_id);
+                return acc;
+            }, {} as Record<string, string[]>);
+            this.tagItemMap = item_tag_relations.reduce((acc, relation) => {
+                if (!acc[relation.tag_id]) {
+                    acc[relation.tag_id] = [];
+                }
+                acc[relation.tag_id].push(relation.item_id);
+                return acc;
+            }, {} as Record<string, string[]>);
+            this.itemList = itemData.map((item) => {
+                const tagList = itemTagMap[item.id].map((id) => tags[id])
+                return {
+                    id: item.id,
+                    name: item.name,
+                    description: item.description,
+                    image: item.image,
+                    steps: item.steps,
+                    tags: tagList
+                }
+            });
+        } catch (error) {
+            throw `データが取得に失敗しました！${error}`
         }
     }
 
+
+    async getAllItems(): Promise<MenuItem[]> {
+        return this.itemList;
+    }
+
+    async getItemById(id: string): Promise<MenuItem | null> {
+        const item = this.itemList.find((item) => item.id === id);
+        if (item === undefined) {
+            return null
+        }
+        return item
+    }
+
     async getItemsByTag(tag: Tag): Promise<MenuItem[]> {
-        const menuItems: MenuItem[] = [
-            {
-                id: '1',
-                name: 'パエリア',
-                description: '',
-                image: '/images/4A29FF84.png',
-                steps: [],
-                tags: [tags['4A29FF84']],
-            },
-            {
-                id: '2',
-                name: 'テスト 料理',
-                description: '',
-                image: '/images/4A29FF84.png',
-                steps: [],
-                tags: [tags['4A29FF84']],  // Corrected: Tagging as breakfast
-            },
-            {
-                id: '3',
-                name: 'Tiramisu',
-                description: 'Classic Italian dessert with layers of coffee-soaked ladyfingers and mascarpone cream',
-                image: '/images/4A29FF84.png',
-                steps: [],
-                tags: [tags['4A29FF84'], tag],  // Corrected: Tagging as a dessert
-            }
-        ];
-        return menuItems;
-    }
-
-    async createItem(item: MenuItem): Promise<MenuItem> {
-        throw `${item.id}`
-    }
-
-    async updateItem(item: MenuItem): Promise<MenuItem> {
-        throw `${item.id}`
-    }
-
-    async deleteItem(id: string): Promise<boolean> {
-        throw `${id}`
+        return this.itemList.filter((item) => item.tags.some((t) => t.id === tag.id));
     }
 }
--- src/menu-client/types/repository.ts
+++ src/menu-client/types/repository.ts
@@ -4,7 +4,4 @@ export interface ItemRepository {
     getAllItems(): Promise<MenuItem[]>;
     getItemById(id: string): Promise<MenuItem | null>;
     getItemsByTag(tag: Tag): Promise<MenuItem[]>;
-    createItem(item: MenuItem): Promise<MenuItem>;
-    updateItem(item: MenuItem): Promise<MenuItem>;
-    deleteItem(id: string): Promise<boolean>;
 }
