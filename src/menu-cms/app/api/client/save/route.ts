4b55dfc8b285d47e28460fe3e8539d93b7e64ac2
i-icc <ichi623110@gmail.com>
2024-11-28 21:37:27 +0900

    #2 command

--- src/menu-cms/app/api/client/save/route.ts
+++ src/menu-cms/app/api/client/save/route.ts
@@ -1,9 +1,9 @@
 import { exec } from 'child_process';
 import { NextResponse } from 'next/server';
 
-export async function GET(): Promise<Response> {
+export async function POST(): Promise<Response> {
     return new Promise((resolve) => {
-        exec('echo HelloWorld', (error, stdout, stderr) => {
+        exec('cp -rf ./public ../menu-client', (error, stderr) => {
             if (error) {
                 console.error(`Error: ${error.message}`);
                 resolve(NextResponse.json({ success: false, error: error.message }));
@@ -14,7 +14,7 @@ export async function GET(): Promise<Response> {
                 resolve(NextResponse.json({ success: false, stderr }));
                 return;
             }
-            resolve(NextResponse.json({ success: true, output: stdout }));
+            resolve(NextResponse.json({ success: true }));
         });
     });
 }
--- src/menu-cms/app/api/github/deploy/route.ts
+++ src/menu-cms/app/api/github/deploy/route.ts
@@ -1,9 +1,9 @@
 import { exec } from 'child_process';
 import { NextResponse } from 'next/server';
 
-export async function GET(): Promise<Response> {
+export async function POST(): Promise<Response> {
     return new Promise((resolve) => {
-        exec('echo HelloWorld', (error, stdout, stderr) => {
+        exec('git add ../menu-client/public; git add ./public; git commit -m "#5 edit sorce";git push', (error, stdout, stderr) => {
             if (error) {
                 console.error(`Error: ${error.message}`);
                 resolve(NextResponse.json({ success: false, error: error.message }));
@@ -14,7 +14,7 @@ export async function GET(): Promise<Response> {
                 resolve(NextResponse.json({ success: false, stderr }));
                 return;
             }
-            resolve(NextResponse.json({ success: true, output: stdout }));
+            resolve(NextResponse.json({ success: true, stdout }));
         });
     });
 }
